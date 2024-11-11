e6daa3f3bb2512f7bf80a1691bfde1349f50b91a
SwatKat1977 <5666718+SwatKat1977@users.noreply.github.com>
2024-11-11 00:37:33 +0000

    update

--- src/DevTest/TestWebserver.cpp
+++ src/DevTest/TestWebserver.cpp
@@ -9,12 +9,18 @@
 #include "WebLoomSettings.h"
 #include "HttpContentType.h"
 #include "core/HttpStatus.h"
+#include "core/Platform.h"
 #include "Request.h"
 #include "RequestMethod.h"
 #include "Response.h"
 #include "RouteHandler.h"
 #include "Templater.h"
 #include "WebLoomExceptions.h"
+
+const std::string PAGE_NOT_FOUND = "<html>"
+                                   "<head><title>Page Not Found</title></head>"
+                                   "<body>Page is not found (404)</body>"
+                                   "</html>";
 
 class AdminWebsite {
 public:
@@ -24,32 +30,35 @@ public:
     }
 
     webloom::Response* HandleLogin (webloom::Request *request) {
-        std::string body = "<html><head><title>I am a teapot</title></head><body>Test</body></html>";
+        webloom::TemplateArguments testArgs = {
+            {"website_name", "Multi-Type Webpage | Index"},
+            {"who", "A.N.Other"} };
+
+        try {
+            return webloom::Templater::Instance().RenderTemplate("test.html",
+                                                                 testArgs);
+        }
+        catch (webloom::TemplateNotFound& ex) {
+            std::cout << "[EXCEPTION] " << ex.what () << "\n";
+            return new webloom::Response(
+                webloom::core::HttpStatus::NotFound,
+                PAGE_NOT_FOUND, webloom::HttpContentType::TextHTML);
+        }
+        catch (webloom::TemplateRenderFailed& ex) {
+            std::cout << "[EXCEPTION] " << ex.what () << "\n";
+            return new webloom::Response (
+                webloom::core::HttpStatus::NotFound,
+                PAGE_NOT_FOUND, webloom::HttpContentType::TextHTML);
+        }
+
         return new webloom::Response(
             webloom::core::HttpStatus::OK,
-            body, webloom::HttpContentType::TextHTML);
+            "OK", webloom::HttpContentType::TextHTML);
     }
 };
 
-#include "RouteHandler.h"
 int main() {
     AdminWebsite test;
-
-#ifdef _TEST_CODE_
-    webloom::Request* re = new webloom::Request(webloom::RequestMethod::Get, webloom::HttpVersion::HTTP_1_0, "/bob");
-
-    auto a = webloom::RouteHandler::Instance().HandleRequest ("invalid", webloom::RequestMethod::Delete, re);
-    if (!a) { printf ("Invalid correct\n"); }
-    printf("[DEBUG] Should return 0 to invalid route : %d\n",
-           webloom::RouteHandler::Instance().IsValidRoute("invalid", webloom::RequestMethod::Head));
-
-    auto b = webloom::RouteHandler::Instance().HandleRequest ("/myroute", webloom::RequestMethod::Get, re);
-    if (b) { printf ("Valid correct\n"); }
-    printf ("[DEBUG] Should return 0 to invalid route : %d\n",
-            webloom::RouteHandler::Instance ().IsValidRoute ("/myroute", webloom::RequestMethod::Head));
-    printf ("[DEBUG] Should return 1 to invalid route : %d\n",
-        webloom::RouteHandler::Instance ().IsValidRoute ("/myroute", webloom::RequestMethod::Put));
-#endif
 
     webloom::WebLoomSettings webloomSettings;
     webloom::Context *context = nullptr;
@@ -69,15 +78,7 @@ int main() {
 
     auto *server = context->CreateServer(loggerSettings);
 
-    try {
-        webloom::Templater::Instance ().RenderTemplate("test.html");
-    }
-    catch(webloom::TemplateNotFound &ex) {
-        std::cout << "[EXCEPTION] " << ex.what () << "\n";
-        return EXIT_FAILURE;
-    }
-
-    //server->Run();
+    server->Run();
 
     return EXIT_SUCCESS;
 }
